a8ef77efdecee99db499867ef064a84b5ea03087
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2023-02-16 13:12:21 +0100

    Creando los componentes para la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoModule } from './todos/todo.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
