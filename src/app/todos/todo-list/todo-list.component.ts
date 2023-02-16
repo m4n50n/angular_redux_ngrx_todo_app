16a44216cc2e36a9b4e93f56388e1d6a7c99c56d
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2023-02-16 14:00:44 +0100

    Desplegando la lista de todos

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { Todo } from '../models/todo.model';
+import { AppState } from 'src/app/app.reducer';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.store.select("todos").subscribe(todos => this.todos = todos);
   }
 
+  ngOnDestroy(): void {
+    // En este caso, el usuario permanecerá en la misma página porque solo hay una, pero si hubiera más, ondestroy deberíamos de cancelar las suscripciones
+  }
 }
